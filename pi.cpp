9d14c37a00e9cca0385a04143999205ca737f3df
MrMai <ericmmai@gmail.com>
2017-04-10 10:05:59 -0700

    super fast c++ caculations ready

--- pi.cpp
+++ pi.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 //  calculates pi by graphing points and then finding the distance between them
-float s = 100000;
+float s = 1000000000;
 long double d = 20;
 
 long double r = d / 2;
@@ -15,10 +15,12 @@ long double res = d / s;
 long double rs = r * r;
 long double ram = 0;
 long double c = 0;
+long double pi = 0;
 
 int main(){
+  cin >> s;
   long double x = 0;
-  while(x < r){
+  while(x < r-(res*2)){
     long double x1 = x;
     long double y1 = sqrt(rs - (x * x));
     x += res;
@@ -26,10 +28,12 @@ int main(){
     long double y2 = sqrt(rs - (x * x));
     x += res;
     ram = pow(pow((x1 - x2), 2) + pow((y1 - y2), 2), 0.5);
-    cout << ram << "\n";
+    //cout << ram << "x\n";
     c += ram;
   }
   c *= 4;
-  cout << c << "\n";
+  pi = c / r;
+  cout << "circumference is " << c << "\n";
+  cout << "pi is " << pi << '\n';
   return 0;
 }
