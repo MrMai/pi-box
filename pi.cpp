0a6809f1d6edfd7b4fedaa1c11d801ddb284aad4
MrMai <ericmmai@gmail.com>
2017-04-10 20:29:59 -0700

    input stuff

--- pi.cpp
+++ pi.cpp
@@ -3,21 +3,30 @@
 #include <iostream>
 //#include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 using namespace std;
 
 //  calculates pi by graphing points and then finding the distance between them
-float s = 100000000;
-long double d = 10;
 
-long double r = d / 2;
-long double res = d / s;
-long double rs = r * r;
-long double ram = 0;
-long double c = 0;
-long double pi = 0;
 
 int main(){
+  // interface stuff
+  float s = 10000000;
+  clock_t t = clock();
+  cout << "enter sample size: ";
+  cin >> s;
+  cout << "calculating with " << s << " samples\n";
+  cout << "predicted finish in " << s / 10000000 * 0.330102 << " seconds\n";
+  // variable declaration
+  long double d = 2;
+  long double r = d / 2;
+  long double res = d / s;
+  long double rs = r * r;
+  long double ram = 0;
+  long double c = 0;
+  long double pi = 0;
+  // calculations
   long double x = 0;
   while(x < r-(res*2)){
     long double x1 = x;
@@ -30,9 +39,16 @@ int main(){
     //cout << ram << "x\n";
     c += ram;
   }
+  // end
   c *= 4;
   pi = c / r;
+  float tr = float((clock() - t)) / CLOCKS_PER_SEC;
   cout << "circumference is " << c << "\n";
   cout << "pi is " << pi << '\n';
+  cout << "calculation took " << tr << " seconds\n";
   return 0;
+}
+
+long double pi(float s) {
+  
 }
